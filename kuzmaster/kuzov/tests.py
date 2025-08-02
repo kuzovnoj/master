c03153014a45c9999918821b0f47121aaf22658a
nout <kuzovnojmaster@gmail.com>
2025-08-02 13:19:46 +0800

    added test_main_page

--- kuzmaster/kuzov/tests.py
+++ kuzmaster/kuzov/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from http import HTTPStatus
+
+
 
 # Create your tests here.
+
+class HomePageTestCase(TestCase):
+    fixtures = ['users_user.json']
+
+    def setUp(self):
+        pass
+
+    def test_main_page_open(self):
+        User = get_user_model()
+        u = User.objects.get(pk=1)
+        self.client.force_login(u)
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def tearDown(self):
+        pass
